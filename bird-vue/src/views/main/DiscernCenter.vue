6198a9f5ff509e612d475178b33397b7a895b2f7
Frank <LemonBoyQi@163.com>
2019-09-06 08:05:44 +0800

    修改样式

--- bird-vue/src/views/main/DiscernCenter.vue
+++ bird-vue/src/views/main/DiscernCenter.vue
@@ -90,13 +90,14 @@ export default {
   width: 200px;
   min-height: 600px;
   margin-right: 0px;
+  z-index: 999;
 }
-/* 内容面板，包含爬虫、图像标记、图像分类和算法评估四大功能 */
-.right-content {
-  float: left;
-  width: 90%;
-  min-height: 400px;
-  margin: 20px 20px;
-  /* background-color: #F2F6FC; */
-}
+  /* 内容面板，包含爬虫、图像标记、图像分类和算法评估四大功能 */
+  .right-content {
+    float: left;
+    width: 90%;
+    min-height: 400px;
+    margin: 20px 20px;
+    /* background-color: #F2F6FC; */
+  }
 </style>
